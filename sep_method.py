3fc9b5c298d5525e19b21b8350999f86fcb5c96e
ganeshreddy2 <67888840+ganeshreddy2@users.noreply.github.com>
2023-01-31 12:31:16 +0530

    Add files via upload

---
+++ sep_method.py
@@ -0,0 +1,12 @@
+a=10
+b=20
+print(a+b, a-b, sep=' ')
+print(a-b, a+b, sep=' ')
+print(a+b, a-b, sep='  ')
+print(a+b, a-b, sep='   ') 
+c="hi"
+d="hello"
+print(c,d, sep=' ')
+print(c,d, sep='  ')
+print(c,d, sep='   ')
+
